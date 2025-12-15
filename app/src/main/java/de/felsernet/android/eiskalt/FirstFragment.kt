01fe6852e00576f5f5c81c962316a50c42dc307e
tfels <32812295+tfels@users.noreply.github.com>
2025-12-15 22:37:30 +0100

    list: add showing a list of strings

--- app/src/main/java/de/felsernet/android/eiskalt/FirstFragment.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/FirstFragment.kt
@@ -1,11 +1,14 @@
 package de.felsernet.android.eiskalt
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import de.felsernet.android.eiskalt.databinding.FragmentFirstBinding
 
 /**
@@ -31,10 +34,33 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+        val adapter = MyAdapter(items)
+        binding.recyclerView.adapter = adapter
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    inner class MyAdapter(private val items: List<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val textView: TextView = itemView.findViewById(R.id.textView)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.textView.text = items[position]
+        }
+
+        override fun getItemCount(): Int = items.size
     }
 }
