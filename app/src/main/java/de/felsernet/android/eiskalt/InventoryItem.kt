7419207c44e8bcfc693935508a772035c9e8c0cf
tfels <32812295+tfels@users.noreply.github.com>
2025-12-21 16:57:27 +0100

    item: add a no-argument constructor for firestore

--- app/src/main/java/de/felsernet/android/eiskalt/InventoryItem.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/InventoryItem.kt
@@ -5,9 +5,12 @@ import kotlin.random.Random
 
 data class InventoryItem(
     var name: String,
-    val id: Long = nextId(),
+    var id: Long = nextId(),
     var quantity: Int = Random.nextInt(0, 11)
 ) : Serializable {
+
+    constructor() : this("", 0L, 0)
+
     companion object {
         private var counter: Long = 1
         private fun nextId(): Long = counter++
