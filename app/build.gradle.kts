fd2078e7b0189496739940469e998a832993225d
tfels <32812295+tfels@users.noreply.github.com>
2026-01-06 16:32:01 +0100

    internal test v0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.felsernet.android.eiskalt"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
