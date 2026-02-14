ea39f17155108a4d3fcace3c33c29184ea7e5ea7
Tim Felser <32812295+tfels@users.noreply.github.com>
2026-02-14 20:18:25 +0100

    row: use same LinearLayout in all row layouts
    resolves #25

--- app/src/main/java/de/felsernet/android/eiskalt/GroupViewHolder.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/GroupViewHolder.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.widget.TextView
 
 class GroupViewHolder(itemView: View) : BaseViewHolder<Group>(itemView) {
-    val textViewGroupName: TextView = itemView.findViewById(R.id.textViewGroupName)
+    val textViewGroupName: TextView = itemView.findViewById(R.id.textViewName)
 
     override fun onBind(holder: BaseViewHolder<Group>, obj: Group) {
         textViewGroupName.text = obj.name
--- app/src/main/java/de/felsernet/android/eiskalt/ItemViewHolder.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/ItemViewHolder.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.widget.TextView
 
 class ItemViewHolder(itemView: View) : BaseViewHolder<Item>(itemView) {
-    val textView: TextView = itemView.findViewById(R.id.textView)
+    val textView: TextView = itemView.findViewById(R.id.textViewName)
 
     override fun onBind(holder: BaseViewHolder<Item>, obj: Item) {
         textView.text = obj.name
