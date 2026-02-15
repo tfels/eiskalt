01b7f624b97dfa1aff1b73091e6858a9731dc710
Tim Felser <32812295+tfels@users.noreply.github.com>
2026-02-15 16:12:57 +0100

    baseViewHolder: just rename obBind --> bind

--- app/src/main/java/de/felsernet/android/eiskalt/ListViewHolder.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/ListViewHolder.kt
@@ -6,8 +6,8 @@ import android.widget.TextView
 class ListViewHolder(itemView: View) : BaseViewHolder<ListInfo>(itemView) {
     val textViewItemCount: TextView = itemView.findViewById(R.id.textViewItemCount)
 
-    override fun onBind(holder: BaseViewHolder<ListInfo>, obj: ListInfo) {
-        super.onBind(holder, obj)
+    override fun bind(holder: BaseViewHolder<ListInfo>, obj: ListInfo) {
+        super.bind(holder, obj)
         textViewItemCount.text = "${obj.itemCount}"
     }
 }
