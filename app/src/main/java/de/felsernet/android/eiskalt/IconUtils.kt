a499e1497ed0191bc266457b627f68ec699ecc1f
tfels <32812295+tfels@users.noreply.github.com>
2026-02-15 22:35:33 +0100

    icon: use a coil ImageRequest.Listener factory

--- app/src/main/java/de/felsernet/android/eiskalt/IconUtils.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/IconUtils.kt
@@ -4,19 +4,27 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import coil.load
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 
 object IconUtils {
+    private fun createCoilLoadListener(imageView: View) = object : ImageRequest.Listener {
+        override fun onSuccess(request: ImageRequest, result: SuccessResult) {
+            imageView.visibility = View.VISIBLE
+        }
+        override fun onError(request: ImageRequest, result: ErrorResult) {
+            imageView.visibility = View.GONE
+            Log.w("icon", "CoilError: ${result.throwable.message}")
+        }
+    }
+
     fun loadAndSetIconAsync(iconInfo: IconInfo, imageViewIcon: ImageView) {
         when (iconInfo.type) {
             IconType.ASSET -> {
                 // Load image from assets handled by coil
                 imageViewIcon.load("file:///android_asset/${iconInfo.path}") {
-                    listener(
-                        onSuccess = { _, _ -> imageViewIcon.visibility = View.VISIBLE },
-                        onError = { _, result -> imageViewIcon.visibility = View.GONE
-                            Log.w("icon", "CoilError: ${result.throwable.message}")
-                        }
-                    )
+                    listener(createCoilLoadListener(imageViewIcon))
                 }
             }
             IconType.UNKNOWN -> {
