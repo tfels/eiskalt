f45b5ce2682823958dc9610cf3fd7511358cc523
tfels <32812295+tfels@users.noreply.github.com>
2026-04-26 11:55:41 +0200

    item: do not allow headers to be swiped

--- app/src/main/java/de/felsernet/android/eiskalt/ItemHeaderViewHolder.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/ItemHeaderViewHolder.kt
@@ -3,4 +3,5 @@ package de.felsernet.android.eiskalt
 import android.view.View
 
 class ItemHeaderViewHolder(itemView: View) : BaseViewHolder<Group>(itemView, iconViewId = null) {
+    override fun isSwipeAllowed(): Boolean = false
 }
