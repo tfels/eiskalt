806477126c7699a5c9f48ed51e0b5088f26cffc5
Tim Felser <32812295+tfels@users.noreply.github.com>
2026-05-17 14:23:43 +0200

    item: show the quantity in the list

--- app/src/main/java/de/felsernet/android/eiskalt/Item.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/Item.kt
@@ -7,7 +7,7 @@ data class Item(
     override var name: String,
     override var id: String = "",
     override var icon: IconInfo? = null,
-    var quantity: Int = Random.nextInt(0, 11),
+    var quantity: Int = 1,
     var groupId: String? = null,
     override var comment: String = ""
 ) : Serializable, BaseDataClass {
--- app/src/main/java/de/felsernet/android/eiskalt/ItemViewHolder.kt
+++ app/src/main/java/de/felsernet/android/eiskalt/ItemViewHolder.kt
@@ -1,8 +1,15 @@
 package de.felsernet.android.eiskalt
 
 import android.view.View
+import android.widget.TextView
 
 class ItemViewHolder(itemView: View,
                      onClick: (item: Item) -> Unit
 ) : BaseViewHolder<Item>(itemView, onClick) {
+    val textViewItemQuantity: TextView = itemView.findViewById(R.id.textViewCount)
+
+    override fun bind(obj: Item) {
+        super.bind(obj)
+        textViewItemQuantity.text = "${obj.quantity}"
+    }
 }
